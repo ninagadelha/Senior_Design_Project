93fdcd1a42ec9edf87a8321e404308b48bc53e10
Alex Moeller <alexmoe@iastate.edu>
2025-02-11 10:47:54 -0600

    fixing a whole lot of weird stuff

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Ensure you have an App.js component in the same directory
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
