5bd27063d7cc87ec55e22abe9ae4e60c0b982475
charliemoreland <cemoreland13@gmail.com>
2025-03-24 13:42:48 -0500

    exisiting user check

--- Backend/api/UserNew/User.router.js
+++ Backend/api/UserNew/User.router.js
@@ -9,7 +9,7 @@ router.post("/login", userController.loginUser);
 router.post("/newuser",userController.newUser);
 router.post("/updateProgramDirector", userController.updateProgramDirector);
 router.post("/updateResearcher", userController.UpdateResearcher)
-
-
+router.post("/updateStudent", userController.UpdateStudent)
+router.get("/userexist", userController.getExistingUser)
 
 module.exports = router;
