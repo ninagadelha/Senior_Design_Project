35adff77b7fd75b9a28efd47af72d0a6baff2c73
Nick Pinnello <npinnello1234@gmail.com>
2025-04-07 11:48:15 -0500

    fix pipeline

--- iinspire-app/src/app/student-home/page.tsx
+++ iinspire-app/src/app/student-home/page.tsx
@@ -2,7 +2,6 @@ import Navbar from "@/components/util/navbar";
 import React from "react";
 import Footer from "@/components/util/footer";
 import StudentDashboardNew from "@/components/dashboards/StudentDashboardNew";
-import { SessionProvider } from "next-auth/react";
 
 const StudentHome = () => {
     return (
