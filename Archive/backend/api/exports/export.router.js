ba545547e1e1739a384fc682bfeccb67fd60f848
charliemoreland <cemoreland13@gmail.com>
2025-04-16 14:46:03 -0500

    Stop tracking Archive folder

--- Archive/backend/api/exports/export.router.js
+++
@@ -1,9 +0,0 @@
-const { generateBySurveyName, generateByProgramName, generateByEmail } = require("./export.controller");
-
-const router = require("express").Router();
-
-router.get("/userEmailCSV", generateByEmail);
-router.get("/avgBySurveyName", generateBySurveyName);
-router.get("/avgByProgramName", generateByProgramName);
-
-module.exports = router;
--- Archive/frontend/__mocks__/NavBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// const MockNavBar = jest.fn(() => <div data-testid="mock-navbar">Mock NavBar</div>);
-
-const MockNavBar = () => {
-    <div data-testid="mock-navbar">Mock NavBar</div>
-  };
-
-export default MockNavBar;
